d589a094ad778007ebec7781cf18f94e3a683379
veweb <walter.rosales@gmail.com>
2016-08-30 22:07:42 -0600

    tercero

---
+++ wp-content/themes/business-turnkey/searchform.php
@@ -0,0 +1,3 @@
+<form method="get" id="searchform" action="<?php bloginfo('url'); ?>/">
+	<input id="search" name="s" type="text" value="Enter search terms and hit enter" onfocus="this.value=''" />
+</form>
